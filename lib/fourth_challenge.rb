b58973f3320076feb61de8f571c84388f417f593
Rochelle Bondoc <rochelleannebaes@gmail.com>
2020-02-20 23:09:46 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -26,6 +26,11 @@ def fourth_challenge
         :name => "Steven",
         :age => "30",
         :attitude => "confused"
+        },
+        {
+        :name => "Nurse",
+        :age => "44",
+        :attitude => "worried"
         }
       ]
    }
